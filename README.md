c2688d77d5c6b51cdf8a11e5a58d3f3d66781b40
maralla <maralla.ai@gmail.com>
2017-01-08 13:26:40 +0800

    Add readme

---
+++ README.md
@@ -0,0 +1,8 @@
+Personal fish config
+
+Install
+-------
+
+```shell
+curl https://raw.githubusercontent.com/maralla/fish-config/master/install.sh | sh
+```
--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env sh
 
+set -e
+
+FISH=~/.config/fish
+
+date=$(date "+%Y-%m-%dT%H:%M:%S")
+
+# backup
+test -d $FISH && mv $FISH $FISH-bak-$date
+
+# clone source
+git clone https://github.com/maralla/fish-config.git $FISH
+
 # install fisher
 curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
 
